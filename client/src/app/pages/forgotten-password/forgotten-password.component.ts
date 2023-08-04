79f8665d1da115d0fdf05fb2b569d6d197f4bd97
judeiwuji <judeiwuji@gmail.com>
2023-08-04 15:05:41 +0100

    Update: implemented FE reset password

--- client/src/app/pages/forgotten-password/forgotten-password.component.ts
+++ client/src/app/pages/forgotten-password/forgotten-password.component.ts
@@ -3,7 +3,10 @@ import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
 import { faArrowLeftLong } from '@fortawesome/free-solid-svg-icons';
 import { ToastrService } from 'ngx-toastr';
+import { ResetPasswordRequest } from 'src/app/models/ResetPassword';
+import { User } from 'src/app/models/User';
 import { ResetPasswordViews } from 'src/app/models/enums/ResetPasswordViews';
+import { UserService } from 'src/app/services/user.service';
 
 @Component({
   selector: 'app-forgotten-password',
@@ -22,8 +25,13 @@ export class ForgottenPasswordComponent implements OnInit {
     confirmPassword: new FormControl('', [Validators.required]),
   });
   faArrowLeftLong = faArrowLeftLong;
+  profile!: User;
 
-  constructor(private router: Router, private toastr: ToastrService) {}
+  constructor(
+    private router: Router,
+    private toastr: ToastrService,
+    private userService: UserService
+  ) {}
 
   ngOnInit(): void {
     this.setView(ResetPasswordViews.Main);
@@ -35,10 +43,43 @@ export class ForgottenPasswordComponent implements OnInit {
   }
 
   identifyUser() {
-    this.setView(ResetPasswordViews.Reset);
+    if (this.processing) return;
+
+    this.processing = true;
+    this.userService
+      .identifyUser(this.accountForm.controls['email'].value as string)
+      .subscribe({
+        next: (response) => {
+          this.processing = false;
+          this.profile = response;
+          this.setView(ResetPasswordViews.Reset);
+        },
+        error: (err) => {
+          this.processing = false;
+          this.toastr.warning(err.error.error || err.statusText);
+        },
+      });
   }
 
   resetPassword() {
-    this.toastr.success('Success');
+    if (this.processing) return;
+
+    this.processing = true;
+    const request: ResetPasswordRequest = {
+      confirmPassword: this.passwordForm.controls['confirmPassword']
+        .value as string,
+      password: this.passwordForm.controls['password'].value as string,
+      userId: this.profile.id,
+    };
+    this.userService.resetPassword(request).subscribe({
+      next: (response) => {
+        this.toastr.success('Success');
+        this.router.navigateByUrl('/login');
+      },
+      error: (err) => {
+        this.processing = false;
+        this.toastr.warning(err.error.error || err.statusText);
+      },
+    });
   }
 }
